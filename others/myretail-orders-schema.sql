3a3b00690aa655b37ecd6a1608b465a3a97d51c6
dipankar-datta <dattapro5@gmail.com>
2019-09-22 20:44:13 +0530

    Added DB schema file.

---
+++ others/myretail-orders-schema.sql
@@ -0,0 +1,128 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.16 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for myretail_orders
+DROP DATABASE IF EXISTS `myretail_orders`;
+CREATE DATABASE IF NOT EXISTS `myretail_orders` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `myretail_orders`;
+
+-- Dumping structure for table myretail_orders.orders
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(120) DEFAULT NULL,
+  `delivery_address` int(11) DEFAULT NULL,
+  `preferred_delivery_date` date DEFAULT NULL,
+  `preferred_time_from` time DEFAULT NULL,
+  `preferred_time_to` time DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `customer` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `FK_orders_contact_details` (`delivery_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.order_product
+DROP TABLE IF EXISTS `order_product`;
+CREATE TABLE IF NOT EXISTS `order_product` (
+  `order` int(20) DEFAULT NULL,
+  `product` int(20) DEFAULT NULL,
+  `quantity` int(20) DEFAULT NULL,
+  `sale_price` double DEFAULT NULL,
+  KEY `FK_order_product_orders` (`order`),
+  KEY `FK_order_product_proucts` (`product`),
+  CONSTRAINT `order_product_ibfk_1` FOREIGN KEY (`order`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.order_status_codes
+DROP TABLE IF EXISTS `order_status_codes`;
+CREATE TABLE IF NOT EXISTS `order_status_codes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) DEFAULT NULL,
+  `code` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_name` (`name`),
+  UNIQUE KEY `unique_code` (`code`),
+  KEY `id` (`id`),
+  KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.order_track
+DROP TABLE IF EXISTS `order_track`;
+CREATE TABLE IF NOT EXISTS `order_track` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `details` varchar(200) DEFAULT NULL,
+  `status_time` datetime DEFAULT NULL,
+  `order` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `FK_order_track_orders` (`order`),
+  KEY `FK_order_track_order_status_codes` (`status`),
+  CONSTRAINT `order_track_ibfk_1` FOREIGN KEY (`status`) REFERENCES `order_status_codes` (`id`),
+  CONSTRAINT `order_track_ibfk_2` FOREIGN KEY (`order`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.product_units
+DROP TABLE IF EXISTS `product_units`;
+CREATE TABLE IF NOT EXISTS `product_units` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product` int(11) NOT NULL DEFAULT '0',
+  `unit` varchar(50) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` double NOT NULL DEFAULT '0',
+  `discount_percentage` float DEFAULT NULL,
+  `discount_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`),
+  KEY `FK__proucts` (`product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.product_unit_map
+DROP TABLE IF EXISTS `product_unit_map`;
+CREATE TABLE IF NOT EXISTS `product_unit_map` (
+  `product` int(11) DEFAULT NULL,
+  `unit` int(11) DEFAULT NULL,
+  KEY `FK__proucts_map` (`product`),
+  KEY `FK__product_units` (`unit`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table myretail_orders.shipment_modes
+DROP TABLE IF EXISTS `shipment_modes`;
+CREATE TABLE IF NOT EXISTS `shipment_modes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(120) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
